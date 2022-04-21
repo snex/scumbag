4adb33e10963efc761a57876164c6b04356a55e1
David Havlicek <snex00@gmail.com>
2022-04-21 16:56:29 -0700

    some code cleanup

--- scumbag.rb
+++ scumbag.rb
@@ -31,6 +31,9 @@ def get_story(scumbag, gender)
       '#collection.a# full of #indefinite_victim.s#',
       '#collection.a# full of #pathetic_adjective# #indefinite_victim.s#'
     ],
+    indifferent_victim: [
+      '#victim#', '#victim_collection#'
+    ],
 
     victim_verb: load_file('victim_verbs'),
 
@@ -40,20 +43,28 @@ def get_story(scumbag, gender)
     object_phys:    load_file('physical_objects'),
     object_nonphys: load_file('non_physical_objects'),
 
-    action: [
-      '#victim_verb# #victim#',
-      '#victim_verb# #victim_collection#',
-
+    singular_transitive_action: [
       '#verb_phys# #victim.pos# #object_phys#',
-      '#verb_phys# #victim_collection.pos# #object_phys.s#',
+      '#verb_nonphys# #victim.pos# #object_nonphys#'
+    ],
+    plural_transitive_action: [
+      '#verb_phys# #victim.pos# #object_phys.s#',
+      '#verb_nonphys# #victim.pos# #object_nonphys.s#'
+    ],
+    transitive_action: [
+      '#singular_transitive_action#',
+      '#plural_transitive_action#'
+    ],
 
-      '#verb_nonphys# #victim.pos# #object_nonphys#',
-      '#verb_nonphys# #victim_collection.pos# #object_nonphys.s#',
+    action: [
+      '#victim_verb# #indifferent_victim#',
+
+      '#transitive_action#',
 
       'borrowed #victim.pos# #object_phys# and never gave it back',
       'borrowed #victim_collection.pos# #object_phys.s# and never gave them back',
 
-      "promised to buy #{rand(10..100)} #object_phys.s# from #victim# but then never paid"
+      "promised to buy #{rand(10..100)} #object_phys.s# from #indifferent_victim# but then never paid"
     ],
 
     story: "I heard #{scumbag} once #action#."
